97fd9ac764a9e61262ed3c5fbd93f4d827602ceb
GelatoBoi <144965276+GelatoBoi@users.noreply.github.com>
2025-01-17 01:26:04 +0000

    Made Cylinder Program

---
+++ CylinderProgram.js
@@ -0,0 +1,7 @@
+let diameter = 2;
+
+let height = 8;
+
+let volume = Math.PI*height*diameter**2;
+
+console.log(`If the cylinders diameter is ${diameter}, and the height is ${height}, therefore, the volume will be ${volume}`);
